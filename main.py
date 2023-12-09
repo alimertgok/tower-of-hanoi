b2d44823d13bee40a1e0e66ec6d64ae542067d20
Ali Mert Gök <gokal@mef.edu.tr>
2023-12-09 17:05:47 +0300

    tower of hanoi recursin added

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello")
-print("Burak")
+# def tower_of_hanoi(no_of_disks, from_disk, to_disk, temp):
+#     if no_of_disks == 1:
+#         print("Move disk tower {} to tower {}".format(from_disk, to_disk))
+#         return
+#     tower_of_hanoi(no_of_disks - 1, from_disk, to_disk, temp)
+#     print("Move disk {} from tower {} to tower {}".format(no_of_disks, from_disk, to_disk))
+#     tower_of_hanoi(no_of_disks - 1, temp, to_disk, from_disk)
+#
+# no_of_disks = int(input("How many disks? "))
+
