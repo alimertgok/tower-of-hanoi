510666eaccbf3978bdeef6f3f09c542718adbd18
Ali Mert Gök <gokal@mef.edu.tr>
2023-12-10 22:08:21 +0300

    Towers added - 10.12.23 22:08

--- Tower.py
+++ Tower.py
@@ -1,30 +1,48 @@
+import turtle
 from turtle import *
 
 
+# class Tower:
+#     pensize(15)
+#
+#     def __init__(self, x, y, width, height, angle):
+#         self.x = x
+#         self.y = y
+#         self.width = width
+#         self.height = height
+#         self.angle = angle
+#
+#     def drawTower(self):
+#         for i in range(3):
+#             penup()
+#             goto(self.x + (i * 300), self.y)
+#             pendown()
+#
+#             forward(self.width)
+#             backward(self.height)
+#             right(self.angle)
+#             forward(self.width)
+#
+#
+# tower_init = Tower(-500, -500, 200, 200, 270)
+#
+# tower_init.drawTower()
+#
+# mainloop()
+
 class Tower:
-    pensize(15)
 
-    def __init__(self, x, y, width, height, angle):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.angle = angle
+    my_screen = Screen()
+    my_screen.screensize(250,200)
 
-    def drawTower(self):
-        for i in range(3):
-            penup()
-            goto(self.x + (i * 300), self.y)
-            pendown()
+    for i in range(3):
+        t = Turtle()
+        t.penup()
+        t.hideturtle()
+        t.speed(100)
+        t.goto(-400 + (i * 400), 0)
+        t.shape("square")
+        t.shapesize(20, 0.5)
+        t.showturtle()
 
-            forward(self.width)
-            backward(self.height)
-            right(self.angle)
-            forward(self.width)
-
-
-tower_init = Tower(-500, -500, 200, 200, 270)
-
-tower_init.drawTower()
-
-mainloop()
+    mainloop()
