d8bfda6c46c99118fd2eab67aa0cdd489f408f73
Ozan <103127960+OzanT1@users.noreply.github.com>
2023-12-12 20:14:29 +0300

    Towers added

--- Tower.py
+++ Tower.py
@@ -1,13 +1,23 @@
+import turtle as t
+
 class Tower(list):
-    def __init__(self, x):
+
+
+    def __init__(self, x, canvas):
         self.x = x
 
-    def push(self, d):
-        d.setx(self.x)
-        d.sety(-150 + 34 * len(self))
-        self.append(d)
+        self.stick = t.RawTurtle(canvas, shape="square")
+        self.stick.penup()
+        self.stick.shapesize(15, 0.5, 2)  # (vertical length, horizontal length, outline)
+        self.stick.speed(1000)
+        self.stick.setx(self.x)
+
+    def push(self, disc):
+        disc.setx(self.x)
+        disc.sety(-150 + 34 * len(self))
+        self.append(disc)
 
     def pop(self):
-        d = list.pop(self)
-        d.sety(150)
-        return d
+        disc = list.pop(self)
+        disc.sety(150)
+        return disc
